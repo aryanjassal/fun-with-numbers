9f7f5baa39c4572f5b18432545969dae39aa43ef
Aryan Jassal <aryanjassal.au@gmail.com>
2022-04-07 10:29:46 +1000

    hello world ncurses now working

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include "utils.hpp"
 
-
 int main() {
     init_ncurses();
 
     clear();
     printw("Hello, world!");
     refresh();
+    getch();
 
     exit_ncurses();
-    return 0;    
+    return 0;
 }
