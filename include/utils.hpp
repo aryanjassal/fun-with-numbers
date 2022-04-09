0a7a638828d726c27b139add21220ef4b91c506c
Aryan Jassal <aryanjassal.au@gmail.com>
2022-04-09 20:36:07 +1000

    cleaned repo

--- include/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-//* Declare the functions here, so when the header is imported, all the corresponding functions are imported from the utils.cpp too
-void init_ncurses();
-void exit_ncurses();
-std::string get_string();
-char get_char();
