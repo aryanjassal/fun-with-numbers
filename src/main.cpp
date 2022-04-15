40dc83c3af7341b67e19eba8a3b05b6d7938c8de
Aryan Jassal <aryanjassal.au@gmail.com>
2022-04-16 00:47:57 +1000

    switched to oof; created basic console color-chaning program

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,11 @@
-#include <ftxui/component/captured_mouse.hpp>
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/component_options.hpp>
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+#define OOF_IMPL
+#include "../include/oof.h"
+
 #include <iostream>
 #include <string>
-#include <vector>
-
-using namespace ftxui;
 
 int main() {
-    auto screen = ScreenInteractive::TerminalOutput();
-    std::vector<std::string> options = {
-        "Check number features",
-        "Plot numbers",
-        "Check overall stats"
-    };
-    int selection = 0;
-
-    MenuOption option;
-    option.on_enter = screen.ExitLoopClosure();
-    auto menu = Menu(&options, &selection, &option);
+    std::cout << oof::fg_color({255, 100, 100}) << "This is red\n";
     
-    screen.Loop(menu);
-    
-    return EXIT_SUCCESS;
+    return 0;
 }
